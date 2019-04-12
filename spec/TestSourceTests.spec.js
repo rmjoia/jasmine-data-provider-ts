ce9a2d237fbbcc2cf392886ae249274b4866fcf2
Ricardo Joia <ricardo.joia@dell.com>
2019-04-12 10:29:10 +0100

    adding strong typed function source test

--- spec/TestSourceTests.spec.js
+++ spec/TestSourceTests.spec.js
@@ -1,6 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var jasmine_data_provider_1 = require("../lib/jasmine-data-provider");
+var SampleClass = /** @class */ (function () {
+    function SampleClass(foo, bar, expected) {
+        this.foo = foo;
+        this.bar = bar;
+        this.expected = expected;
+    }
+    return SampleClass;
+}());
 describe("parameterised test using a function as source", function () {
     function provider() {
         return [
@@ -10,10 +18,22 @@ describe("parameterised test using a function as source", function () {
             { a: "22", b: "13", expected: "2213" }
         ];
     }
+    function sampleClassProvider() {
+        return [
+            new SampleClass("foo1", "bar1", "foo1bar1"),
+            new SampleClass("f00", "bar", "f00bar")
+        ];
+    }
     jasmine_data_provider_1.TestSource(provider, function (data) {
         it("should return value using operator +", function () {
             var result = data.a + data.b;
             expect(result).toEqual(data.expected);
+        });
+    });
+    jasmine_data_provider_1.TestSource(sampleClassProvider, function (data) {
+        it("'" + (data.foo +
+            data.bar) + "' and '" + data.expected + "' should be equal", function () {
+            expect(data.foo + data.bar).toEqual(data.expected);
         });
     });
 });
--- spec/TestSourceTests.spec.ts
+++ spec/TestSourceTests.spec.ts
@@ -1,7 +1,11 @@
 import { TestSource } from "../lib/jasmine-data-provider";
 
+class SampleClass {
+  constructor(public foo: any, public bar: any, public expected: string) {}
+}
+
 describe("parameterised test using a function as source", function() {
-  function provider() {
+  function provider(): any[] {
     return [
       { a: 2, b: 3, expected: 5 },
       { a: "14", b: 15, expected: "1415" },
@@ -9,11 +13,25 @@ describe("parameterised test using a function as source", function() {
       { a: "22", b: "13", expected: "2213" }
     ];
   }
-  
+
   TestSource<any>(provider, function(data) {
     it("should return value using operator +", function() {
       var result = data.a + data.b;
       expect(result).toEqual(data.expected);
+    });
+  });
+
+  function sampleClassProvider(): SampleClass[] {
+    return [
+      new SampleClass("foo1", "bar1", "foo1bar1"),
+      new SampleClass("f00", "bar", "f00bar")
+    ];
+  }
+
+  TestSource<SampleClass>(sampleClassProvider, function(data) {
+    it(`'${data.foo +
+      data.bar}' and '${data.expected}' should be equal`, function() {
+      expect(data.foo + data.bar).toEqual(data.expected);
     });
   });
 });
